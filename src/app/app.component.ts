cd6eacec618071342f3c119e0c6d8219d233883e
Gourish Vashisht <gourish.vashisht@nagarro.com>
2021-03-28 14:59:30 +0530

    - add rxjs operators examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { combineLatest, from, fromEvent, Observable, of, Subscription, timer } from 'rxjs';
-import { delay, tap, filter, reduce } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { from, fromEvent, of } from 'rxjs';
+import { delay, filter, map, reduce, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +14,52 @@ export class AppComponent implements OnInit {
   ngOnInit() {
 
   }
+
+  rxjsOperatiorsExamples() {
+    // 1. of
+    // const observable = of(10, 20, 30, 40, 50);
+    // observable.subscribe(
+    //   next => console.log('next:', next),
+    //   err => console.log('erro: ', err),
+    //   () => console.log('complete')
+    // )
+
+    // 2. from
+    // const array = [1, 2, 3, 4, 5];
+    // const observable2 = from(array);
+    // observable2.subscribe(
+    //   next => console.log(next)
+    // )
+
+    // 3. delay
+    // const clicks = fromEvent(document, 'click');
+    // const delayedClicks = clicks.pipe(delay(1000));
+    // delayedClicks.subscribe(x => console.log(x));
+
+    // 4. map
+    // const clicks = fromEvent(document, 'click');
+    // const delayedClicks = clicks.pipe(map((event: any) => event.clientX));
+    // delayedClicks.subscribe(x => console.log(x));
+
+    // 5. tap
+    // const clicks = fromEvent(document, 'click');
+    // const delayedClicks = clicks.pipe(
+    //   tap(event => console.log(event)),
+    //   map((event: any) => event.clientX)
+    // );
+    // delayedClicks.subscribe(x => console.log(x));
+
+    // 6. filter
+
+    // const values = from([1, 2, 3, 4, 5]);
+    // values.pipe(filter(value => value % 2 == 0))
+    //   .subscribe(x => console.log(x));
+
+    // 7. reduce
+    // const values = from([1, 2, 3, 4, 5]);
+    // const initialValue = 0;
+    // values.pipe(reduce((acc, value) => acc + value, initialValue))
+    //   .subscribe(x => console.log(x));
+  }
+
 }
